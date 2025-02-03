8b613a9875609234ceacc312fbc6e5ef7482e90b
asagulenko <a.sagulenko@GMAIL.COM>
2025-02-03 13:55:04 -0500

    Added solutions

---
+++ session_01/Longest_Alph_order_sequence.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  1 14:07:36 2025
+
+@author: anna
+"""
+
+"""
+Assume s is a string of lower case characters.
+
+Write a program that prints the longest substring of s in which 
+the letters occur in alphabetical order. 
+For example, if s = 'azcbobobegghakl', then your program should print
+
+Longest substring in alphabetical order is: beggh
+"""
+
+import string
+
+s=('azcbobobegghakl') #initial string
+n_string=s[0] #temporary sequence string for comparison
+longest_string=s[0] #longest sequence in alphabetical order
+
+for char in s[1:]:
+    if string.ascii_letters.index(char)>=string.ascii_letters.index(n_string[-1]):
+        n_string+=char
+        if len(longest_string)<len(n_string):
+            longest_string=n_string
+    else: n_string=char
+print (longest_string)
